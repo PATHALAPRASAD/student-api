a61a548dda12f085df547572a5f68274d36f0508
pathalaprasad <pathalaprasad2016jag@gmail.com>
2026-01-11 11:55:14 +0530

    Modified StudentController.java and application.properties

--- src/main/java/com/example/student_api/controller/StudentController.java
+++ src/main/java/com/example/student_api/controller/StudentController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/api/students")
 @CrossOrigin("*")
 @Tag(name="Student Services", description = "Student API")
 @Validated
